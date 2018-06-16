59f174552f3ea3dc4262dd95219b1800cba096c9
Unknown <liemnt.uit@gmail.com>
2018-06-16 15:22:49 +0700

    fix route

--- BackpackingItemFrontend/src/components/components.layouts/Route.js
+++ BackpackingItemFrontend/src/components/components.layouts/Route.js
@@ -5,23 +5,39 @@ import Home from "../../pages/pages.home/Home";
 import OrderPage from '../../pages/pages.order/OrderPage'
 import {Loading} from "./index";
 import ListPageContainer from "../../pages/pages.list/ListPageContainer";
-import {Route, Switch,Redirect} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
-export default class MyRoute extends React.Component{
-    constructor(props){
+export default class MyRoute extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={};
+        this.state = {};
     }
-    render(){
+
+    render() {
         return (
             <Switch>
                 <Route exact path={'/'} component={Home}/>
-                <Route exact path={'/order'} component={OrderPage}/>
-                <Route exact path={'/product/:productId'} component={DetailPageContainer}/>                {
-                    this.props.userInfo==null?<Redirect to="/"></Redirect>:
-                        <Route exact path={'/profile'} component={ProfileContainer}/>
-                }
+
+                <Route exact path={'/product/:productId'} component={DetailPageContainer}/>
+                <Route exact path={'/order'} render={() => {
+                    if (this.props.userInfo) {
+                        return (
+                            <OrderPage/>
+                        )
+                    }
+                    alert("Vui lòng đăng nhập để đặt hàng")
+                    return (<Redirect to={'/'}></Redirect>)
+                }}/>
+                <Route exact path={'/profile'} render={() => {
+                    if (this.props.userInfo) {
+                        return (
+                            <ProfileContainer/>
+                        )
+                    }
+                    return (<Redirect to={'/'}></Redirect>)
+                }}/>
                 <Route exact path={'/:categorySlug/:subCategorySlug?'} component={ListPageContainer}/>
+
                 <Route exact path={'/loading'} component={Loading}/>
 
             </Switch>
